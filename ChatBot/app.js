624a301779ab1e6bb32689b554df648e1d7d810f
Gianella A. Achetoni <gianellaachetoni@gmail.com>
2024-05-12 22:42:24 -0300

    Update de cosas importantes

--- ChatBot/app.js
+++ ChatBot/app.js
@@ -1,11 +1,5 @@
-
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+import { GoogleGenerativeAI } from "@google/generative-ai";
 const clave = require("./Clave.JS");
-
-document.getElementById("send-button").addEventListener("click", function() {
-    var textoIngresado = document.getElementById("textoEntrada").value;
-    // Acá se guarda el texto
-});// esto lo voy a mover a otro archivo
 
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(clave); //Aca le paso la clave Api key
@@ -15,7 +9,7 @@ const genAI = new GoogleGenerativeAI(clave); //Aca le paso la clave Api key
 const model = genAI.getGenerativeModel({ model: "MODEL_NAME"}); 
 
 
-async function run() {
+async function run(msg) {
     // For text-only input, use the gemini-pro model
     const model = genAI.getGenerativeModel({ model: "gemini-pro"});
   
@@ -34,18 +28,9 @@ async function run() {
         maxOutputTokens: 100,
       },
     });
-    //Crear el boton para salir del chat y listo        POR HACER
-    //Agregar un while para que se repita el texto      POR HACER
-
-    const msg = textoIngresado ;
     //textoingresado llama a la variable de arriba creada y traida del index
-  
     const result = await chat.sendMessage(msg);
     const response = await result.response;
     const text = response.text();
     console.log(text);
   }
-  
-  run();
-
-
