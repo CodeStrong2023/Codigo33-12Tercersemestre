3edc6282c233cdb163c327cb906b5548bbf55dfd
Simon Carabajal <135768121+SimonCarabajal@users.noreply.github.com>
2024-06-19 23:43:28 -0300

    Clase 4 java ClaseHija

---
+++ JAVA/Clase 4 Java/ClaseHija2.java
@@ -0,0 +1,12 @@
+public class ClaseHija2 extends Clase2{
+
+    public ClaseHija2 () {
+        super();
+        this.atributoDefault = "Modificacion del Atributo Default"
+        System.out.println("atributoDefault = " + this.atributoDefault);
+        this.metodoDefault () ;
+    }
+
+
+
+}
