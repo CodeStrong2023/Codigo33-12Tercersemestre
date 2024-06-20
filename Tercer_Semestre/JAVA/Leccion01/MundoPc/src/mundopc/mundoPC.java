f75c6733f4cfb1bc6b51ee68dc950e629c8589b5
Ezequiel Andrés Flores <136276268+EzeFlores1988@users.noreply.github.com>
2024-06-19 23:03:18 -0300

    Semana-1
    Lección-1

---
+++ Tercer_Semestre/JAVA/Leccion01/MundoPc/src/mundopc/mundoPC.java
@@ -0,0 +1,34 @@
+
+package mundopc;
+
+import ar.com.system2023.mundopc.*;
+
+public class mundoPC {
+    public static void main(String[] args) {
+        Monitor monitorHP = new Monitor("HP", 13); //Importar la clase
+        Teclado tecladoHP = new Teclado("BluetoothHP", "HP");
+        Raton ratonHP = new Raton("Bluetooth", "HP");
+        Computadora computadoraHP = new Computadora("Computadora HP", monitorHP, tecladoHP, ratonHP);
+        
+        //Creamos otros objetos de diferentes marcas
+        Monitor monitorGamer = new Monitor("Gamer", 32);
+        Teclado tecladoGamer = new Teclado("Bluetooth", "Gamer");
+        Raton ratonGamer = new Raton("Bluetooth", "Gamer");
+        Computadora computadoraGamer = new Computadora("Computadora Gamer", monitorGamer, tecladoGamer, ratonGamer);
+        Orden orden1 = new Orden(); //Inicializamos el arreglo vacio
+        Orden orden2 = new Orden(); //Una nueva lista para el objeto orde2
+        orden1.agregarComputadora(computadoraHP);
+        orden1.agregarComputadora(computadoraGamer);
+        orden1.mostrarOrden();
+        
+        Computadora computadorasVarias = new Computadora("Computadora de diferentes marcas", monitorHP, tecladoGamer, ratonHP);
+        orden2.agregarComputadora(computadorasVarias);
+        
+        orden1.mostrarOrden();
+        orden2.mostrarOrden();
+        
+        //Crear mas objetos de tipo computadora con todos sus elementos
+        //Completar una lista en el objeto orde1 que llegue a los 10 elementos
+        //Probar de esta manera los metodos al maximo rendimiento
+    }
+}
