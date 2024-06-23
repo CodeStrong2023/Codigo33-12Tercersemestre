4c534db0988f926e5e4e2e41cc21fb9177dfe099
Ezequiel Andrés Flores <136276268+EzeFlores1988@users.noreply.github.com>
2024-06-23 12:43:40 -0300

    Lección-5
    Semana-5

---
+++ Tercer_Semestre/JAVA/Leccion05/ConversionObjetos/src/domain/Escritor.java
@@ -0,0 +1,26 @@
+
+package domain;
+
+public class Escritor extends Empleado{
+    final TipoEscritura tipoEscritura;
+    
+    public Escritor(String nombre, double sueldo, TipoEscritura tipoEscritura1){
+        super(nombre, sueldo);
+        this.tipoEscritura = tipoEscritura1;
+    }
+    
+    //Metodo para sobreescribir
+    @Override
+    public String obtenerDetalles(){
+        return super.obtenerDetalles()+", Tipo Escritura: "+tipoEscritura.getDescripcion();
+    }
+    
+    @Override
+    public String toString(){
+        return "Escritor{" + "tipoEscritura=" + tipoEscritura + '}'+" "+super.toString();
+    }
+    
+    public TipoEscritura getTipoEscritura(){
+        return this.tipoEscritura;
+    }
+}
