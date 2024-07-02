bee144dce281cdf26092b491a248cef89423128f
ArriolaBelen <arbel421@gmail.com>
2024-07-02 01:03:10 -0300

    Archivos ChatBot

---
+++ ChatBot/palabrasalerta.js
@@ -0,0 +1,7 @@
+
+const palabrasClave = ["Me gustaría estar muerto","Desearía no haber nacido","A veces, quisiera desaparecer",
+"Un día de estos me van a encontrar muerto","Se van a librar de mí","Ya no quiero vivir más","Los demás van a estar mejor sin mí",
+"La única solución es matarme","Ya no puedo estar con este dolor", "Me quiero morir", "Estoy pensando en suicidarme",
+"matarme", "suicidarme", "morirme"
+];
+
