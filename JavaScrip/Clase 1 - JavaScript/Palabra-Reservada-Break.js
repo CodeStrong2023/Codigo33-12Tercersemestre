9fcbd4c07cd96821af75807c04e9653804e761fc
Jessica Pagano <jeessieh@hotmail.com>
2024-07-01 21:08:34 -0300

    Semana 4
    Carga archivos & orden carpeta

--- JavaScrip/Clase 1 - JavaScript/Palabra-Reservada-Break.js
+++
@@ -1,8 +0,0 @@
-// Palabra reservada break
-for(let contando = 0; contando <= 10; contando++){
-    if(contando % 2 == 0){
-        console.log(contando); //Muestra todos los pares
-        break;
-    }
-}
-console.log("Termina el ciclo al encontrar el primer numero par");
